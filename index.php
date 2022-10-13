86313a632348bdd4695f077bb92886350f58ae9f
Xeus <nsaghakingsly@gmail.com>
2022-10-13 05:39:19 +0100

    scraper works want to create a fallback function should incase it lands on 404

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+/**
+ *
+ * Class For Crawling the Websites
+ */
 class Scraping {
     public string $base_url;
     public function __construct(){
@@ -11,14 +16,14 @@ class Scraping {
         $nodes = $finder->query("//*[contains(@class, '$classname')]");
 
         $csv_values = array();
-        foreach ($nodes as $node) {
-
-//            var_dump($node);
+        foreach ($nodes as $k => $node) {
             $link_val = $node->getElementsByTagName('a');
             $link = $link_val[0]->getAttribute('href');
-            $value = $this->grabCourseDetails($link);
+            $value = $this->grabCourseDetails($link, $category_name);
             array_push($csv_values, $value);
-
+           if($k == 4) {
+               break;
+           }
         }
         var_dump( $csv_values);
         return $csv_values;
@@ -32,7 +37,7 @@ class Scraping {
         return new DomXPath($DOM);
     }
 
-    public function grabCourseDetails($link){
+    public function grabCourseDetails($link, $category_name){
         $finder = $this->createDom($this->base_url . $link);
         $header = $finder->query("//h1[1]/text()[1]");
         $course_name = $header[0]->nodeValue;
@@ -50,61 +55,73 @@ class Scraping {
         if($rating[0] == null) $rating_value = '';
         else $rating_value = str_replace(' ratings', '', $rating[0]->nodeValue);
         $rating_value = str_replace(',', '', $rating_value);
-        var_dump($rating_value);
-        return [
-            'course_name' => $course_name,
-            'instructor_name' => $instructor_name,
-            'description' => $final_content,
-            'enrolled' => $enrolled_value,
-            'rating' => $rating_value
-        ];
+        return [$category_name, $course_name, $instructor_name,$final_content,$enrolled_value,$rating_value];
     }
 
 }
 
+/**
+ * Class To Generate the CVS File
+ *
+ */
 class CSVGenerator {
     public Scraping $scraping;
     public function __construct(Scraping $scraping){
         $this->scraping = $scraping;
     }
 
-    public function generateCSV($data){
+    public function generateCSV($category){
+        $data = $this->scraping->getCategoryCourses($category);
+        var_dump($data);
+        header('Content-Type: text/csv');
+        header('Content-Disposition: attachment; filename="work.csv"');
 
+        array_unshift($data, array('Category Name', 'Course Name', 'First Instructor Name', 'Course Description', '# of Students Enrolled', '# of Ratings'));
+
+        $fp = fopen("work.csv", "w");
+        foreach ($data as $line) {
+            fputcsv($fp, $line, ',');
+        }
+        fclose($fp);
+        return $fp;
     }
 }
-//return new CSVGenerator(new Scraping('business'));
-$courses = new Scraping();
-return $courses->getCategoryCourses('business');
 
+class ClientSide {
+    public CSVGenerator $csvGenerator;
+    public function __construct(CSVGenerator $csvGenerator){
+        $this->csvGenerator = $csvGenerator;
+    }
 
-//$base_url = "https://www.coursera.org";
-//
-//$finder = createDom($base_url . '/browse/business');
-//$classname = 'rc-BrowseProductCard';
-//$nodes = $finder->query("//*[contains(@class, '$classname')]");
-//
-//$csv_values = array();
-//foreach ($nodes as $node) {
-//
-//    var_dump($node->nodeValue);
-//    $link_val = $node->getElementsByTagName('a');
-//    $link = $link[0]->getAttribute('href');
-//
-//}
-//return 'ok';
-//$handle = fopen("C:\Users\Stephen\Documents\WorkCSV\work.csv", "w");
-//if (false !== $handle) {
-//    fputcsv($handle, $csv_values);
-//}
-//
-//function createDom($link){
-//    $html = file_get_contents($link);
-//    $DOM = new DOMDocument();
-//    libxml_use_internal_errors(true);
-//    $DOM->loadHTML($html);
-//    return new DomXPath($DOM);
-//}
-//
-//function getCourseDetails($link){
-//    createDom($b)
-//}
+    public function webUser(){
+        ?>
+        <html>
+        <body>
+        <div align="center">
+            <form action="" method="POST">
+                <b>Category Name:</b><input type="text" name="category_name"><br>
+                <input type="submit">
+            </form>
+        </div>
+        </body>
+        </html>
+        <?php
+    }
+
+    public function submitForm(){
+        if (empty($_POST["category_name"])) {
+            $errMsg = "Error! You didn't enter the Category Name.";
+            echo $errMsg;
+            return $this->webUser();
+        } else {
+            $category_name = $_POST['category_name'];
+        }
+        return $this->csvGenerator->generateCSV($category_name);
+    }
+}
+
+$client = new ClientSide(new CSVGenerator(new Scraping()));
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    return $client->submitForm();
+}
+else return $client->webUser();
